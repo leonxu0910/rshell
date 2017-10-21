51f5552aeb28adf05d1b493e751c89f88ed03a0d
Patrick Porter <pport004@ucr.edu>
2017-10-21 22:30:22 +0000

    Added user input parsing functionality

--- src/ExecShell.cpp
+++ src/ExecShell.cpp
@@ -1,6 +1,10 @@
 #include "../header/ExecShell.h"
 #include <string>
+#include <vector>
+#include <cstring>
+
 using std::string;
+using std::vector;
 
 ExecShell::ExecShell() { this->userInput = ""; }
 
@@ -13,5 +17,21 @@ void ExecShell::execute() {
 }
 
 void ExecShell::parseLine() {
-    
+    vector<string> vToken;
+    char* cstr = new char [userInput.length() + 1];
+    cstr = strcpy(cstr, userInput.c_str());
+    char* tok = strtok(cstr, " ");
+    while(tok != NULL) {
+        string element = tok;
+        string semiColon = "";
+        if(element.at(element.size()-1) == ';') {
+            element = element.substr(0, element.size()-1);
+            semiColon = ";";
+        }
+        vToken.push_back(element);
+        if(semiColon == ";") {
+            vToken.push_back(semiColon);
+        }
+        tok = strtok(NULL, " ");
+    }
 }
