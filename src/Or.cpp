ae2e455e5d39b6864cd5bb6c287d890dc33c6c3f
leonxu0910 <leonxu0910@hotmail.com>
2017-10-22 15:52:11 +0000

    Implemented Or execution

--- src/Or.cpp
+++ src/Or.cpp
@@ -1,7 +1,18 @@
 #include "../header/Or.h"
 
 bool Or::execute() {
+    bool done;
+    if (status == false) {
+        if (next != 0) {
+            next->execute();
+        }
+        else {
+            // error: no right operand
+        }
+    }
+    else {
+        // previous is true, dont execute
+    }
     
-    
-    return true;
+    return done;
 }
